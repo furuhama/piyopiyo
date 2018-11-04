c929c9d175364a58739cf550d9c8cff6a0ddc608
furuhama <furuhama@users.noreply.github.com>
2018-11-04 20:43:13 +0900

    Create minipost.rb prototype

---
+++ minipost.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+time = Time.now
+minipost_title = time.strftime('%Y-%m-%d_%H%M%S') + '.markdown'
+minipost_directory = File.expand_path('../_posts/miniposts', __FILE__)
+target_path = minipost_directory + '/' + minipost_title
+
+raise RuntimeError.new "Already exists #{minipost_title}" if File.exist? target_path
+
+File.open(target_path, 'w') do |f|
+  f.puts '---'
+  f.puts 'layout: post'
+  f.puts 'title: ' # TODO: Set title from argument
+  f.puts "date: #{time.strftime('%Y-%m-%d %H:%M:%S %z')}"
+  f.puts 'tag: [minipost]'
+  f.puts '---'
+end
+
+puts "Created: #{minipost_title}"
