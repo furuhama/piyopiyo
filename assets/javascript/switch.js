e60821a6b1c73fbaccf321dc8ed08c106f03f2fe
furuhama <furuhama@users.noreply.github.com>
2018-11-08 22:22:36 +0900

    Make functions to toggle colors

--- assets/javascript/switch.js
+++ assets/javascript/switch.js
@@ -4,8 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
   checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
       console.log('Checked');
+      toggleColorToLight(document.body);
     } else {
       console.log('Not checked');
+      toggleColorToDark(document.body);
     }
   });
 });
+
+function toggleColorToLight(element) {
+  element.className += " dark";
+};
+
+function toggleColorToDark(element) {
+  element.className -= " dark";
+};
