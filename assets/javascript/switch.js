a90390e4f8172371e235425853f969379a24d325
furuhama <furuhama@users.noreply.github.com>
2018-11-10 15:41:08 +0900

    Use cookie to save the color mode statement

--- assets/javascript/switch.js
+++ assets/javascript/switch.js
@@ -1,21 +1,51 @@
+// Confirm cookie value on load
+window.onload = function() {
+  var cookieNightModeArray = document.cookie.split(';').filter((item) => item.includes('night='));
+
+  if (cookieNightModeArray.length !== 0 && cookieNightModeArray[0].includes('true')) {
+    document.querySelector('input[type="checkbox"]').checked = true;
+
+    toggleColorToDark();
+  };
+};
+
+// Toggle colormode
 document.addEventListener('DOMContentLoaded', function () {
   var checkbox = document.querySelector('input[type="checkbox"]');
 
   checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
       console.log('Checked');
-      toggleColorToLight(document.body);
+
+      document.cookie = "night=true; path=/";
+
+      toggleColorToDark();
     } else {
       console.log('Not checked');
-      toggleColorToDark(document.body);
+
+      document.cookie = "night=false; path=/";
+
+      toggleColorToLight();
     }
   });
 });
 
-function toggleColorToLight(element) {
+function toggleColorToDark() {
+  console.log('Set all colors to dark');
+
+  toggleElementColorToDark(document.body);
+};
+
+function toggleColorToLight() {
+  console.log('Set all colors to light');
+
+  toggleElementColorToLight(document.body);
+};
+
+function toggleElementColorToDark(element) {
   element.className += " dark";
 };
 
-function toggleColorToDark(element) {
+function toggleElementColorToLight(element) {
   element.className -= " dark";
 };
